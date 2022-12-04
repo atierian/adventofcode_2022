6cd8b30383e79da3787f567620d27e26171ad865
Ian Saultz <52051793+atierian@users.noreply.github.com>
2022-12-04 11:35:39 -0500

    Update day4.swift

--- day4/day4.swift
+++ day4/day4.swift
@@ -47,11 +47,3 @@ let partTwo = sets
 	}
 
 print("partTwo", partTwo)
-
-extension Array {
-	func chunked(into size: Int) -> [[Element]] {
-			stride(from: 0, to: count, by: size).map {
-				Array(self[$0..<Swift.min($0 + size, count)])
-			}
-	}
-}
